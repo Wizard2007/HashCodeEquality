7afdf6d4f7537b68a3440a04c666015b235975b7
Eujine <sponsor85@mail.ru>
2019-03-03 00:27:56 +0200

    [+] comparers for array and overload quals and get hashcode functions.

---
+++ ArrayHashCodeEquality/ArrayHashCodeEquality/ArrayHashCodeEquality.cs
@@ -0,0 +1,50 @@
+﻿using ArrayHashCodeEquality.Comparers;
+using System;
+using System.Collections.Generic;
+
+namespace ArrayHashCodeEquality
+{
+    class ArrayHashCodeEquality
+    {
+        static void Main(string[] args)
+        {
+
+            int[] x = { 1, 2, 3 };
+            int[] y = { 1, 2, 3 };
+            int[] z = { 4, 5, 6 };
+
+            var comparer = new ArrayEqualityComparer<int>();
+
+            Console.WriteLine(comparer.GetHashCode(x));
+            Console.WriteLine(comparer.GetHashCode(y));
+            Console.WriteLine(comparer.GetHashCode(z));
+            Console.WriteLine(comparer.Equals(x, y));
+            Console.WriteLine(comparer.Equals(x, z));
+
+
+            Console.WriteLine();
+            Console.WriteLine("Byte comparers");
+            byte[] x1 = { 1, 2, 3 };
+            byte[] y1 = { 1, 2, 3 };
+            byte[] z1 = { 4, 5, 6 };
+
+            var comparer1 = new ByteArrayEqualityComparer();
+
+            Console.WriteLine(comparer1.GetHashCode(x1));
+            Console.WriteLine(comparer1.GetHashCode(y1));
+            Console.WriteLine(comparer1.GetHashCode(z1));
+            Console.WriteLine(comparer1.Equals(x1, y1));
+            Console.WriteLine(comparer1.Equals(x1, z1));
+
+            var t = new Dictionary<byte[], string>(comparer1);
+
+            t.Add(x1, "x");
+            t.Add(z1, "z");
+
+            Console.WriteLine($"t[y] = {t[y1]} ");
+
+            Console.WriteLine("Press [Enter] for continue ...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayHashCodeEquality/ArrayHashCodeEquality/Comparers/ByteArrayEqualityComparer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace ArrayHashCodeEquality.Comparers
+{
+    public sealed class ByteArrayEqualityComparer : IEqualityComparer<byte[]>
+    {
+        // You could make this a per-instance field with a constructor parameter
+        private static readonly EqualityComparer<byte> elementComparer
+            = EqualityComparer<byte>.Default;
+
+        public bool Equals(byte[] first, byte[] second)
+        {
+            if (first == second)
+            {
+                return true;
+            }
+            if (first == null || second == null)
+            {
+                return false;
+            }
+            if (first.Length != second.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (!elementComparer.Equals(first[i], second[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int GetHashCode(byte[] array)
+        {
+            unchecked
+            {
+                if (array == null)
+                {
+                    return 0;
+                }
+                int hash = 17;
+                foreach (byte element in array)
+                {
+                    hash = hash * 31 + elementComparer.GetHashCode(element);
+                }
+                return hash;
+            }
+        }
+    }
+}
